ba5e403fd85c0df66740152fe648c0ef00cd4fda
Ramanpreet Aujla <ramanpreetaujla@Ramanpreets-Mac-mini.local>
2026-03-28 15:05:00 -0400

    feat: CROWN-179 - add storybook crown decorator

--- apps/web/.storybook/preview.ts
+++ apps/web/.storybook/preview.ts
@@ -1,6 +1,24 @@
+import * as React from 'react';
+
 import type { Preview } from '@storybook/experimental-nextjs-vite';
 
+import { PlatformThemeProvider } from '../components/platform/platform-theme-provider';
+
+const withCrownPreview: NonNullable<Preview['decorators']>[number] = (Story) =>
+  React.createElement(
+    PlatformThemeProvider,
+    null,
+    React.createElement(
+      'div',
+      {
+        className: 'min-h-screen p-6',
+      },
+      React.createElement(Story),
+    ),
+  );
+
 const preview: Preview = {
+  decorators: [withCrownPreview],
   parameters: {
     controls: {
       matchers: {
