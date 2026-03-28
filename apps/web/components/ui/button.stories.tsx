abe1d039fa90ac61b9f67813d6de3408029c8f85
Ramanpreet Aujla <ramanpreetaujla@Ramanpreets-Mac-mini.local>
2026-03-28 15:09:02 -0400

    feat: CROWN-179 - add starter button story

---
+++ apps/web/components/ui/button.stories.tsx
@@ -0,0 +1,14 @@
+import { Button } from './button';
+
+const meta = {
+  title: 'Button',
+  component: Button,
+};
+
+export default meta;
+
+export const Default = {
+  args: {
+    children: 'Button',
+  },
+};
