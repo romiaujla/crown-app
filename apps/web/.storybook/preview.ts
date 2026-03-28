ac850c9e6ce9d5c5c61530705025883e76744f32
Ramanpreet Aujla <ramanpreetaujla@Ramanpreets-Mac-mini.local>
2026-03-28 15:03:29 -0400

    feat: CROWN-179 - add storybook preview scaffold

---
+++ apps/web/.storybook/preview.ts
@@ -0,0 +1,18 @@
+import type { Preview } from '@storybook/experimental-nextjs-vite';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    layout: 'centered',
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+};
+
+export default preview;
